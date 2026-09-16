742cad57cb4dd16130242934dd9e5c0593b0a7c1
Snowsearcher <usser0718@gmail.com>
2026-09-16 00:23:36 -0500

    Enable hardened SnowBunny CYOA renderer

--- public/scripts/extensions/snowbunny-mobile/bootstrap.js
+++ public/scripts/extensions/snowbunny-mobile/bootstrap.js
@@ -1,6 +1,7 @@
 import { initChatStatistics } from './chat-statistics.js';
 import { initComposerLayout } from './composer-layout.js';
 import { initContextView } from './context-view.js';
+import { initCyoaNative } from './cyoa-native.js';
 import { init as initMobileShell } from './index.js';
 import {
     currentMessageIdentity,
@@ -64,6 +65,7 @@ export function init() {
     initModelSelector();
     initPersonaSelector();
     initScenario();
+    initCyoaNative();
     initChatStatistics();
     initShellTransitions();
 }
