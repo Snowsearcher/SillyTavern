1804ac21f0d35e37ad9cf059d13528679e110517
Snowsearcher <usser0718@gmail.com>
2026-09-15 22:44:51 -0500

    Initialize SnowBunny state namespace

--- public/scripts/extensions/snowbunny-mobile/bootstrap.js
+++ public/scripts/extensions/snowbunny-mobile/bootstrap.js
@@ -1,7 +1,9 @@
 import { init as initMobileShell } from './index.js';
 import { initMessageIdentity } from './message-identity.js';
+import { initSnowBunnyState } from './state.js';
 
 export function init() {
+    initSnowBunnyState();
     initMessageIdentity();
     initMobileShell();
 }
